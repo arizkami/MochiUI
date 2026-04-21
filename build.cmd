6af18debd450d6680120deedd3eb87ab4e888701
arizkami <amcpethai22@gmail.com>
2026-04-21 17:26:03 +0700

    fix complier

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
 python scripts/gen_theme.py
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
-cmake --build build
+cmake -S . -G "Ninja" -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j%NUMBER_OF_PROCESSORS%
